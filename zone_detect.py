76767b16153c8d6be3f53523b390fc42b5d6bb5c
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-04-30 08:51:32 +0700

    update

--- zone_detect.py
+++ zone_detect.py
@@ -5,12 +5,12 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 
-def draw_rectangle(frame, xyxy, names, clas, cof):
+def draw_rectangle(frame, xyxy, names, cls, cof):
     # 1 ====== draw cv2.putText() method ======
     # font
     font = cv2.FONT_HERSHEY_SIMPLEX
-
     # org
+    # print("xyxy", xyxy)
     org = (int(xyxy[0][0]), int(xyxy[0][1]))
 
     # fontScale
@@ -22,29 +22,19 @@ def draw_rectangle(frame, xyxy, names, clas, cof):
     # Line thickness of 2 px
     thickness = 2
 
-    # Using cv2.putText() method
     if int(xyxy[0][1]) < int(frame.shape[0]/2):
-        frame = cv2.putText(frame, names[clas[0]] + " " + str(round(cof[0], 2)),
+        # Using cv2.putText() method
+        frame = cv2.putText(frame, names[cls[0]] + " " + str(round(cof[0], 2)),
                             org, font, fontScale, color, thickness, cv2.LINE_AA)
 
-    # 2 ============ resize image =============================
-    scale_percent = 60  # percent of original size
-    width = int(frame.shape[1] * scale_percent / 100)
-    height = int(frame.shape[0] * scale_percent / 100)
-    dim = (width, height)
-
-    # resize image
-    resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-
-    # 3 ====== draw cv2.rectangle() method ======
-
+    # 2 ====== draw cv2.rectangle() method ===================
     # Start coordinate, here (5, 5)
     # represents the top left corner of rectangle
     start_point = (5, 5)
 
     # Ending coordinate, here (220, 220)
     # represents the bottom right corner of rectangle
-    end_point = (resized.shape[1], int(resized.shape[0]/2))
+    end_point = (int(frame.shape[1]), int(frame.shape[0] / 2))
 
     # Blue color in BGR
     color = (255, 0, 0)
@@ -54,10 +44,18 @@ def draw_rectangle(frame, xyxy, names, clas, cof):
 
     # Using cv2.rectangle() method
     # Draw a rectangle with blue line borders of thickness of 2 px
-    image = cv2.rectangle(resized, start_point, end_point, color, thickness)
+    image = cv2.rectangle(frame, start_point, end_point, color, thickness)
 
-    # Displaying the image
-    cv2.imshow("window_name", image)
+    # 3 ============ resize image =============================
+    scale_percent = 60  # percent of original size
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    # resize image
+    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+
+    cv2.imshow("zone", resized)
 
 
 # Open the video file
@@ -84,18 +82,12 @@ while cap.isOpened():
 
         for result in results:
             boxes = result.boxes.numpy()  # Boxes object for bbox outputs
-            # print("list 1", boxes.xyxy)
             names = result.names
-            # list_2 = []
             for box in boxes:  # there could be more than one detection
                 # print("class", box.cls)
                 # print("xyxy", box.xyxy)
                 # print("conf", box.conf)
-
-                # if box.xyxy[0][1] < 200:
-                #     list_2.append(box.xyxy[0])
                 draw_rectangle(frame, box.xyxy, names, box.cls, box.conf)
-            # print("list_2", list_2)
 
     else:
         # Break the loop if the end of the video is reached

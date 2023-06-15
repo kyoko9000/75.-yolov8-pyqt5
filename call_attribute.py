b0f121383c5bc39d8d435ae9812799b3d57861ce
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-06-15 20:51:21 +0700

    update

--- call_attribute.py
+++ call_attribute.py
@@ -5,9 +5,11 @@ class a():
         self.list_2 = [3, 4, 5]
         for i in range(1, 3):
             # print(eval(f'self.list_{i}'))
-            # print(getattr(self, f'list_{i}'))
+            # x = getattr(self, f'list_{i}')
+            # print(x)
             setattr(self, f'list_{i}', [i, i, i])
-            print(getattr(self, f'list_{i}'))
+            x = getattr(self, f'list_{i}')
+            print(x)
 
 
 a()

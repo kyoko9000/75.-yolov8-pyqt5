40a45120c48afae939761192aadbde016885c28e
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-06-23 20:34:40 +0700

    update

--- call_attribute.py
+++ call_attribute.py
@@ -3,17 +3,18 @@ class a:
         super().__init__()
         self.list_1 = [0, 1, 2]
         self.list_2 = [3, 4, 5]
-        for i in range(1, 3):
+        # for i in range(1, 3):
             # print(eval(f'self.list_{i}'))
             # x = getattr(self, f'list_{i}')
             # print(x)
-            setattr(self, f'list_{i}', [i, i, i])
-            x = getattr(self, f'list_{i}')
-            print(x)
+            # setattr(self, f'list_{i}', [i, i, i])
+            # x = getattr(self, f'list_{i}')
+            # print(x)
 
 
-a()
-# for i in range(1, 3):
-#     setattr(a, f'list_{i}', [i, i, i])
-#     print(getattr(a, f'list_{i}'))
-    # print(getattr(a(), f'list_{i}'))
+# a()
+for i in range(1, 3):
+    # setattr(a, f'list_{i}', [i, i, i])
+    # print(getattr(a, f'list_{i}'))
+    x = getattr(a(), f'list_{i}')
+    print(x)

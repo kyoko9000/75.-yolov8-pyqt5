59f5b1f254856709ee8b8b62e7ae9ad88d331a40
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-07-09 17:38:51 +0700

    update

--- yolov8_pose.py
+++ yolov8_pose.py
@@ -6,9 +6,11 @@ model = YOLO('yolov8n-pose.pt')  # load an official model
 # Predict with the model
 results = model('video.mp4', show=True, stream=True)  # predict on an image
 for result in results:
-    boxes = result[0].boxes.numpy()  # Boxes object for bbox outputs
-    for box in boxes:  # there could be more than one detection
-        print(box)
-        # print("class", box.cls)
-        # print("xyxy", box.xyxy)
-        # print("conf", box.conf)
+    keypoints = result[0].keypoints.numpy()
+    for keypoint in keypoints:
+        print(keypoint.xy[0][0])
+    # boxes = result[0].boxes.numpy()  # Boxes object for bbox outputs
+    # for box in boxes:  # there could be more than one detection
+    #     print("class", box.cls)
+    #     print("xyxy", box.xyxy)
+    #     print("conf", box.conf)

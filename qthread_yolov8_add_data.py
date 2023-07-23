f4e4c7ce7be0d8ce1e970b7cc15825eb6ea8ee27
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-07-23 16:58:48 +0700

    update

--- qthread_yolov8_add_data.py
+++ qthread_yolov8_add_data.py
@@ -1,6 +1,5 @@
 import sys
 import cv2
-import numpy as np
 from PyQt5.QtCore import Qt, QRect
 from PyQt5 import QtGui
 from PyQt5.QtCore import QThread, pyqtSignal
@@ -29,7 +28,7 @@ class mylabel(QLabel):
             self.x1 = event.x()
             self.y1 = event.y()
             self.update()
-            print("move", self.x1, self.y1)
+            # print("move", self.x1, self.y1)
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -55,12 +54,12 @@ class MainWindow(QMainWindow):
     def mouseReleaseEvent(self, event):
         if self.take:
             data = [self.lb.y0, self.lb.y1, self.lb.x0, self.lb.x1]
-            self.uic.label_2.setPixmap(self.qt_img)
             self.thread[1].take_pic(data)
 
     def take_pics(self):
         data = [165, 202, 90, 127]
         self.thread[1].take_pic(data)
+        # show cursor
         self.lb = mylabel(self)
         self.lb.setGeometry(QRect(20, 0, 331, 251))
         self.lb.setCursor(Qt.CrossCursor)
@@ -72,10 +71,11 @@ class MainWindow(QMainWindow):
         self.thread[1].signal.connect(self.show_wedcam)
         self.thread[1].signal_1.connect(self.show_pic)
 
-    def show_pic(self, pic, data):
+    def show_pic(self, pic):
         self.take = True
-        print(data)
-        self.qt_img = self.convert_cv_qt(pic)
+        """Updates the image_label with a new opencv image"""
+        qt_img = self.convert_cv_qt(pic)
+        self.uic.label_2.setPixmap(qt_img)
 
     def show_wedcam(self, cv_img):
         """Updates the image_label with a new opencv image"""
@@ -90,13 +90,13 @@ class MainWindow(QMainWindow):
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
         convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(lebel_w, label_h, Qt.KeepAspectRatio)
+        p = convert_to_Qt_format.scaled(lebel_w, label_h)  # , Qt.KeepAspectRatio
         return QPixmap.fromImage(p)
 
 
 class live_stream(QThread):
     signal = pyqtSignal(object)
-    signal_1 = pyqtSignal(object, object)
+    signal_1 = pyqtSignal(object)
 
     def __init__(self, index):
         self.data = None
@@ -110,14 +110,18 @@ class live_stream(QThread):
         results = model('video1.mp4', show=True, stream=True)  # List of Results objects
 
         for result, frame in results:
-            # boxes = result[0].boxes.numpy()  # Boxes object for bbox outputs
-            # for box in boxes:  # there could be more than one detection
-            #     print("class", box.cls)
-            #     print("xyxy", box.xyxy)
-            #     print("conf", box.conf)
             self.signal.emit(frame)
             if self.pic:
-                self.signal_1.emit(frame, self.data)
+                print(frame.shape)
+                print(self.data)
+
+                y0 = int(self.data[0]*3)
+                y1 = int(self.data[1]*3)
+                x0 = int(self.data[2]*4.1)
+                x1 = int(self.data[3]*4.1)
+
+                crop_img = frame[y0:y1, x0:x1]
+                self.signal_1.emit(crop_img)
                 self.pic = False
 
     def take_pic(self, data):

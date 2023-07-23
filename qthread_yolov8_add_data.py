8a5472e0168069363ad37f0529bb5c5314e3073e
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-07-23 11:53:16 +0700

    update

--- qthread_yolov8_add_data.py
+++ qthread_yolov8_add_data.py
@@ -1,19 +1,49 @@
 import sys
 import cv2
 import numpy as np
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRect
 from PyQt5 import QtGui
 from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPixmap, QPainter, QPen
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from ultralytics import YOLO
 
 from gui1 import Ui_MainWindow
 
 
+class mylabel(QLabel):
+    y0 = 0
+    x0 = 0
+    x1 = 0
+    y1 = 0
+    flag = False
+
+    def mousePressEvent(self, event):
+        self.flag = True
+        self.x0 = event.x()
+        self.y0 = event.y()
+        print("start cap", self.x0, self.y0)
+
+    def mouseMoveEvent(self, event):
+        if self.flag:
+            self.x1 = event.x()
+            self.y1 = event.y()
+            self.update()
+            print("move", self.x1, self.y1)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.red, 2, Qt.SolidLine))
+        rec = QRect(self.x0, self.y0, abs(self.x1 - self.x0), abs(self.y1 - self.y0))
+        painter.drawRect(rec)
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.qt_img = None
+        self.take = False
+        self.lb = None
         self.uic = Ui_MainWindow()
         self.uic.setupUi(self)
 
@@ -22,8 +52,19 @@ class MainWindow(QMainWindow):
 
         self.thread = {}
 
+    def mouseReleaseEvent(self, event):
+        if self.take:
+            data = [self.lb.y0, self.lb.y1, self.lb.x0, self.lb.x1]
+            self.uic.label_2.setPixmap(self.qt_img)
+            self.thread[1].take_pic(data)
+
     def take_pics(self):
-        self.thread[1].take_pic(11)
+        data = [165, 202, 90, 127]
+        self.thread[1].take_pic(data)
+        self.lb = mylabel(self)
+        self.lb.setGeometry(QRect(20, 0, 331, 251))
+        self.lb.setCursor(Qt.CrossCursor)
+        self.lb.show()
 
     def start_capture_video(self):
         self.thread[1] = live_stream(index=1)
@@ -31,9 +72,10 @@ class MainWindow(QMainWindow):
         self.thread[1].signal.connect(self.show_wedcam)
         self.thread[1].signal_1.connect(self.show_pic)
 
-    def show_pic(self, pic):
-        qt_img = self.convert_cv_qt(pic)
-        self.uic.label_2.setPixmap(qt_img)
+    def show_pic(self, pic, data):
+        self.take = True
+        print(data)
+        self.qt_img = self.convert_cv_qt(pic)
 
     def show_wedcam(self, cv_img):
         """Updates the image_label with a new opencv image"""
@@ -54,7 +96,7 @@ class MainWindow(QMainWindow):
 
 class live_stream(QThread):
     signal = pyqtSignal(object)
-    signal_1 = pyqtSignal(object)
+    signal_1 = pyqtSignal(object, object)
 
     def __init__(self, index):
         self.data = None
@@ -75,8 +117,7 @@ class live_stream(QThread):
             #     print("conf", box.conf)
             self.signal.emit(frame)
             if self.pic:
-                print(self.data)
-                self.signal_1.emit(frame)
+                self.signal_1.emit(frame, self.data)
                 self.pic = False
 
     def take_pic(self, data):

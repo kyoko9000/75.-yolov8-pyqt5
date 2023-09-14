4903b55d9d45a881aef9f698344c446763cf3a48
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-09-14 19:58:30 +0700

    update

--- save_file.py
+++ save_file.py
@@ -7,20 +7,31 @@ from gui2 import Ui_MainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.path = None
         self.uic = Ui_MainWindow()
         self.uic.setupUi(self)
-        self.uic.pushButton.clicked.connect(self.save)
+        self.uic.pushButton.clicked.connect(self.open)
+        self.uic.pushButton_2.clicked.connect(self.save)
+
+    def open(self):
+        # default_path = "D:/2. Python projects"
+        default_path = ""
+        self.path, _ = QFileDialog.getOpenFileName(None, "open file", default_path, "*.txt")
+        print(self.path)
 
     def save(self):
-        filename = "filename"
-        path = QFileDialog.getSaveFileName(None, "save file", filename, ".txt")
-        if len(path) == 0:
-            return
-        path_str = ''.join(path)
+        try:
+            filename = self.path  # filename or default path to file
+            path, _ = QFileDialog.getSaveFileName(None, "save file", filename, "*.txt")
+            print(path)
+            if len(path) == 0:
+                return
 
-        text_file = open(path_str, "w")
-        text_file.write("self.__console.toPlainText()")
-        text_file.close()
+            text_file = open(path, "w")
+            text_file.write("test message")
+            text_file.close()
+        except:
+            pass
 
 
 if __name__ == "__main__":
